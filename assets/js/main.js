e372cf99975e5b14ace7265574cc25114aa8e71a
valindo godinho <valindo.godinho@gmail.com>
2016-02-26 11:09:46 +0530

    Stable state for routes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 var app = angular.module('velmenniApp',['ui.router']);
 
-app.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise("/");
-	$stateProvider.state("/",{
+app.config(function($stateProvider, $urlRouterProvider,$locationProvider) {
+	// $urlRouterProvider.otherwise("index");
+	$stateProvider.state("index",{
 		url: "/",
 		templateUrl: "partials/video.html"
+	}).state("products",{
+		url: "/products",
+		templateUrl: "partials/products.html"
 	});
+	$locationProvider.html5Mode(true);
 });
 
 
